434be054cd5e350e9cac523979b082e9917fcecf
Josh <joshx@gmx.eu>
2013-04-04 00:04:05 +0200

    added config file for global application settings

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ var express			 = require('express')
   , http			 = require('http')
   , fs				 = require('fs')
   , pg				 = require('pg')
-  , path			 = require('path');
+  , path			 = require('path')
+  , config			 = require('./config');
 
 
 var app = express();
-var conString = "postgres://calendar:asdf@localhost:5432/calendar";
-//postgres://user:password@host:port/database
 function isInArray(value, array) {
 	for (var i=0; i<array.length; i++) {
 		if (array[i] === value) {
@@ -18,9 +17,9 @@ function isInArray(value, array) {
 	}
 	return false;
 }
-var key = 'MIICXAIBAAKBgQChm75ObmHa7tjxfNHsJ8orMACCjurZbQDqpJFPXiYXbTlOhP+LDi8cl1WmK4Y9vKNgMSAu3tCvy3kb8qGh/yCLYHEVnz678dM2K8yRW7uc/4VAztwkYOTfoydwmdA1C1og7CiiQtvkwoTxwv3kGlFb18whJee2YOBQ0B6GXGFy6wIDAQABAoGABYfyDHckrDyOej1eZem6tp2u9sjzarubU2yMeJ3tSdH4KyLMKDM1E5JuYQCOWKCTKuCjjFcd51Zcb8NvGr9DmtOjJpBLKmrDrKzg2XLGipAIsgMLG0TpUzAuMhj1D8bNmLu/CPO+RndEx3mF85u531fr5KtypDYv8R4ogyyCJnkCQQDU8CKKgSLaRLUz3GnBl9HSsq053sTdGq5fXXBBnaWqdjvmbEDRgtIlk93B4Mu/dQJ9pDWalD17kDtyK6Tx+msdAkEAwkpByDA4bfHdAriI4tf4iMjBfkOHdZHMp/Qy406qySpA3DfK7UUwkWLl3DM4zjG2ZNt/a+dtY0xIQkG1W6RvpwJBAJEW+oIjUYsly84Ffm3xs398Tbojx0HcvzmtoiKjd1E59MChvFzFZclDApPrRwkygjr326pzHZ2G/mphwKc8eSUCQDKIB6XeTL7jmdy8S/Xbv+src4+4VoHQgs7n51hRPIAHekkMRb4CMciOVUQ5Gjwel9aRdAmHbl7WFzEMT/Pex58CQCtIeFjbrzUq8hWkDP35HlnLjHRCfeFER+4xLaFsPedjIaPmfnKEd8oHNh3WmptHFPsgOWpG8+ygOYpt7FRuxYA=';
+
 function getEvents(req, res) {
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		res.writeHead(200, {'Content-Type' : 'application/json'});
 		var query = client.query('SELECT * FROM calendar');
 		var response = [];
@@ -39,14 +38,14 @@ function getEvents(req, res) {
 app.engine('.html', require('ejs').__express);
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || config.web.port || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'html');
   app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(expressValidator);
-  app.use(express.cookieParser(key));
+  app.use(express.cookieParser(config.web.secureKey));
   app.use(express.session());
   app.use(express.csrf());
   app.use(express.static(path.join(__dirname, 'public')));
@@ -62,7 +61,7 @@ process.on('uncaughtException', function(err) {
 });
 
 app.get('/', function(req, res){
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		var query = client.query("SELECT * FROM users WHERE username = '" + req.signedCookies.username + "' AND pass = '" + req.signedCookies.password + "'");
 		var users = [];
 		query.on('row', function(row) {
@@ -80,7 +79,7 @@ app.get('/', function(req, res){
 	});
 });
 app.get('/home', function(req, res){
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		var query = client.query("SELECT * FROM users WHERE username = '" + req.signedCookies.username + "' AND pass = '" + req.signedCookies.password + "'");
 		var users = [];
 		query.on('row', function(row) {
@@ -121,7 +120,7 @@ app.post('/setCookie', function(req, res){
 	    return;
 	}
 	
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		var query = client.query("SELECT * FROM users WHERE username = '" + req.body.username + "' AND pass = '" + req.body.password + "'");
 		var users = [];
 		query.on('row', function(row) {
@@ -150,7 +149,7 @@ app.get('/destroySession', function(req, res){
 });
 app.post('/addEvent', function(req, res){
 	
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		var query = client.query("INSERT INTO calendar (title, description, start_date, end_date, start_time, end_time) VALUES ('" + req.body.title + "', '" + req.body.description + "', '" + req.body.start_date + "', '" + req.body.end_date + "', '" + req.body.start_time + "', '" + req.body.end_time + "')");
 		done();
 	});
@@ -166,7 +165,7 @@ app.get('/getCSRFToken', function(req, res){
 });
 app.get('/getEvents', function(req, res){
 	console.log("/getEvents");
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		var query = client.query("SELECT * FROM users WHERE username = '" + req.signedCookies.username + "' AND pass = '" + req.signedCookies.password + "'");
 		var users = [];
 		query.on('row', function(row) {
@@ -190,7 +189,7 @@ app.get('/getEvents', function(req, res){
 });
 app.post('/updateEvent', function(req, res){
 	
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		var query = client.query("UPDATE calendar SET title='" + req.body.newTitle + "', description='" + req.body.newDescription + "', start_date='" + req.body.newStart_date + "', end_date='" + req.body.newEnd_date + "', start_time='" + req.body.newStart_time + "', end_time='" + req.body.newEnd_time + "' WHERE id=" + req.body.id);
 		done();
 	});
@@ -201,7 +200,7 @@ app.post('/updateEvent', function(req, res){
 });
 app.post('/delEvent', function(req, res){
 	
-	pg.connect(conString, function(err, client, done){
+	pg.connect(config.db.url, function(err, client, done){
 		var query = client.query("DELETE FROM calendar WHERE id = " + req.body.id);
 		query.on('end', function(){
 			done();
