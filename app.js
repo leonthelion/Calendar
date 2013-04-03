d55a3826f3ba7e206f5f9359484c390d91b7c0e6
luca leon junk <lucaleonjunk@web.de>
2013-04-03 21:54:45 +0200

    see description
    -login input validation (backend)
    -csrf protection

--- app.js
+++ app.js
@@ -19,6 +19,22 @@ function isInArray(value, array) {
 	return false;
 }
 var key = 'MIICXAIBAAKBgQChm75ObmHa7tjxfNHsJ8orMACCjurZbQDqpJFPXiYXbTlOhP+LDi8cl1WmK4Y9vKNgMSAu3tCvy3kb8qGh/yCLYHEVnz678dM2K8yRW7uc/4VAztwkYOTfoydwmdA1C1og7CiiQtvkwoTxwv3kGlFb18whJee2YOBQ0B6GXGFy6wIDAQABAoGABYfyDHckrDyOej1eZem6tp2u9sjzarubU2yMeJ3tSdH4KyLMKDM1E5JuYQCOWKCTKuCjjFcd51Zcb8NvGr9DmtOjJpBLKmrDrKzg2XLGipAIsgMLG0TpUzAuMhj1D8bNmLu/CPO+RndEx3mF85u531fr5KtypDYv8R4ogyyCJnkCQQDU8CKKgSLaRLUz3GnBl9HSsq053sTdGq5fXXBBnaWqdjvmbEDRgtIlk93B4Mu/dQJ9pDWalD17kDtyK6Tx+msdAkEAwkpByDA4bfHdAriI4tf4iMjBfkOHdZHMp/Qy406qySpA3DfK7UUwkWLl3DM4zjG2ZNt/a+dtY0xIQkG1W6RvpwJBAJEW+oIjUYsly84Ffm3xs398Tbojx0HcvzmtoiKjd1E59MChvFzFZclDApPrRwkygjr326pzHZ2G/mphwKc8eSUCQDKIB6XeTL7jmdy8S/Xbv+src4+4VoHQgs7n51hRPIAHekkMRb4CMciOVUQ5Gjwel9aRdAmHbl7WFzEMT/Pex58CQCtIeFjbrzUq8hWkDP35HlnLjHRCfeFER+4xLaFsPedjIaPmfnKEd8oHNh3WmptHFPsgOWpG8+ygOYpt7FRuxYA=';
+function getEvents(req, res) {
+	pg.connect(conString, function(err, client, done){
+		res.writeHead(200, {'Content-Type' : 'application/json'});
+		var query = client.query('SELECT * FROM calendar');
+		var response = [];
+		query.on('row', function(row) {
+			response.push({ id : row.id, title : row.title, description : row.description, start_date : row.start_date, end_date : row.end_date, start_time : row.start_time, end_time : row.end_time, allDay : row.allday});
+		});
+
+		query.on('end', function() {
+			res.write(JSON.stringify(response));
+			done();
+			res.end();
+		});
+	});
+}
 
 app.engine('.html', require('ejs').__express);
 
@@ -29,8 +45,10 @@ app.configure(function(){
   app.use(express.favicon(__dirname + '/public/images/favicon.ico'));
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
+  app.use(expressValidator);
   app.use(express.cookieParser(key));
   app.use(express.session());
+  app.use(express.csrf());
   app.use(express.static(path.join(__dirname, 'public')));
   app.use(app.router);
 });
@@ -46,7 +64,6 @@ process.on('uncaughtException', function(err) {
 app.get('/', function(req, res){
 	pg.connect(conString, function(err, client, done){
 		var query = client.query("SELECT * FROM users WHERE username = '" + req.signedCookies.username + "' AND pass = '" + req.signedCookies.password + "'");
-		console.log(req.signedCookies);
 		var users = [];
 		query.on('row', function(row) {
 			users.push({username : row.username, password : row.pass});
@@ -65,7 +82,6 @@ app.get('/', function(req, res){
 app.get('/home', function(req, res){
 	pg.connect(conString, function(err, client, done){
 		var query = client.query("SELECT * FROM users WHERE username = '" + req.signedCookies.username + "' AND pass = '" + req.signedCookies.password + "'");
-		console.log(req.signedCookies);
 		var users = [];
 		query.on('row', function(row) {
 			users.push({username : row.username, password : row.pass});
@@ -75,7 +91,6 @@ app.get('/home', function(req, res){
 			done();
 			if (users.length === 1) {
 				var data = fs.readFileSync('./views/index.html', 'utf-8');
-				res.writeHead(200, {'Content-Type' : 'text/html'});
 				res.write(data);
 				res.end();
 			} else {
@@ -85,21 +100,33 @@ app.get('/home', function(req, res){
 	});
 });
 app.get('/login', function(req, res){
-	var data = fs.readFileSync('./views/login.html', 'utf-8');
-	res.writeHead(200, {'Content-Type' : 'text/html'});
-	res.write(data);
-	res.end();
+	res.render('login', {
+		token : req.session._csrf
+	});
 });
 app.post('/setCookie', function(req, res){
+	
+	req.assert('username', 'invalid username').len(5, 25).notContains(' ').isAlphanumeric();
+	req.assert('password', 'invalid password').len(4, 16).notContains(' ').isAlphanumeric();
+	
+	var errors = req.validationErrors();
+	if (errors) {
+		if (errors[0].param === 'username' && errors[errors.length-1].param === 'username') {
+			res.redirect('/login?username=' + req.body.username + '&password=' + req.body.password + '&usernameStatus=invalid&passwordStatus=valid');
+		} else if (errors[0].param === 'password') {
+			res.redirect('/login?username=' + req.body.username + '&password=' + req.body.password + '&usernameStatus=valid&passwordStatus=invalid');
+		} else if (errors[0].param === 'username' && errors[errors.length-1].param === 'password') {
+			res.redirect('/login?username=' + req.body.username + '&password=' + req.body.password + '&usernameStatus=invalid&passwordStatus=invalid');
+		}
+	    return;
+	}
+	
 	pg.connect(conString, function(err, client, done){
-		console.log(req.body.username);
-		console.log(req.body.password);
 		var query = client.query("SELECT * FROM users WHERE username = '" + req.body.username + "' AND pass = '" + req.body.password + "'");
 		var users = [];
 		query.on('row', function(row) {
 			users.push({username : row.username, password : row.pass});
 		});
-		console.log(users);
 		query.on('end', function() {
 			done();
 			if (users.length === 0) {
@@ -114,7 +141,8 @@ app.post('/setCookie', function(req, res){
 		});
 	});
 });
-app.post('/destroySession', function(req, res){
+app.get('/destroySession', function(req, res){
+	req.session.destroy();
 	res.clearCookie('username');
 	res.clearCookie('password');
 	res.redirect('/');
@@ -131,23 +159,34 @@ app.post('/addEvent', function(req, res){
 	res.writeHead(200, {'Content-Type' : 'text/html'});
 	res.end();
 });
-app.post('/getEvents', function(req, res){
-	
+app.get('/getCSRFToken', function(req, res){
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.write(JSON.stringify({ token: req.session._csrf }));
+	res.end();
+});
+app.get('/getEvents', function(req, res){
+	console.log("/getEvents");
 	pg.connect(conString, function(err, client, done){
-		res.writeHead(200, {'Content-Type' : 'application/json'});
-		var query = client.query('SELECT * FROM calendar');
-		var response = [];
+		var query = client.query("SELECT * FROM users WHERE username = '" + req.signedCookies.username + "' AND pass = '" + req.signedCookies.password + "'");
+		var users = [];
 		query.on('row', function(row) {
-			response.push({ id : row.id, title : row.title, description : row.description, start_date : row.start_date, end_date : row.end_date, start_time : row.start_time, end_time : row.end_time, allDay : row.allday});
+			users.push({username : row.username, password : row.pass});
 		});
-
+		
 		query.on('end', function() {
-			res.write(JSON.stringify(response));
 			done();
-			res.end();
+			if (users.length === 1) {
+				access = true;
+			} else {
+				res.redirect('/login');
+			}
 		});
 	});
-
+	setTimeout(function(){
+		if (access===true) {
+			getEvents(req, res);
+		}
+	}, 250);
 });
 app.post('/updateEvent', function(req, res){
 	
@@ -164,7 +203,9 @@ app.post('/delEvent', function(req, res){
 	
 	pg.connect(conString, function(err, client, done){
 		var query = client.query("DELETE FROM calendar WHERE id = " + req.body.id);
-		done();
+		query.on('end', function(){
+			done();
+		});
 	});
 	
 	console.log('event removed');
@@ -172,9 +213,7 @@ app.post('/delEvent', function(req, res){
 	res.end();
 });
 app.all('*', function(req, res) {
-	res.writeHead(404, {'Content-Type' : 'text/html'});
-	res.write(fs.readFileSync('./views/error/404.html', 'utf-8'));
-	res.end();
+	res.render('error/404');
 });
 
 http.createServer(app).listen(app.get('port'), function(){
